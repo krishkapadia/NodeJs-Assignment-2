45f74161da3fe722df2c75b060a5f7502400ddd6
KRISH <krishkapadia1999@gmail.com>
2020-11-21 12:21:10 +0530

    Task_3 done

--- Task_3.js
+++ Task_3.js
@@ -1,1 +1,37 @@
 // Session Login
+const express = require('express');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("login", { success: null });
+});
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'keyboard cat'
+}));
+
+app.post("/", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username == "admin" && password == "admin") {
+        
+        req.session.uname = username;
+        res.render("login_success", { success: "Successfull Login..." });
+    }
+    else {
+        res.render("login", { success: "Invalid Username or Password !" });
+    }
+
+});
+
+app.listen(port);
