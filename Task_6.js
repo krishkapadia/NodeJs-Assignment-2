7ac6120e571cdb473c2df369f513fa49f58e5e51
KRISH <krishkapadia1999@gmail.com>
2020-11-21 17:37:52 +0530

    Crud Done in Task_6.js

--- Task_6.js
+++ Task_6.js
@@ -24,18 +24,17 @@ const tblLoginSchema = new mongoose.Schema({
     password: String
 });
 
+const tblStudentSchema = new mongoose.Schema({
+    name: String,
+    surname: String,
+    email: String,
+    city: String
+})
+
+
 // Referencing Schema
 const tblLogin = mongoose.model('tblLogin', tblLoginSchema);
-
-// Inserting Demo record for login
-// var tmp = new tblLogin({
-//     username : "admin",
-//     password:"admin"
-// });
-
-// tmp.save((err,data)=>{
-//     console.log("Insert Successfull");
-// })
+const tblStudent = mongoose.model('tblStudent',tblStudentSchema);
 
 // Set View Engine
 app.set("view engine", "ejs");
@@ -45,7 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Get Method
 app.get("/", (req, res) => {
-    res.render("login", { success: null, color: null});
+    res.render("login", { success: null, color: null });
 });
 
 app.post("/", (req, res) => {
@@ -54,13 +53,74 @@ app.post("/", (req, res) => {
     var password = req.body.password;
     tblLogin.findOne({ username: username, password: password }, (err, data) => {
         if (data == null)
-            res.render("login", { success: "Invalid Username or Password" , color : "Red"});
+            res.render("login", { success: "Invalid Username or Password", color: "Red" });
         else
-            res.render("login", { success: "Successfull Login", color: "Green"});
+            res.render("student", { success: "Successfull Login", color: "Green" });
+    });
+
+});
+
+app.post("/student", (req, res) => {
+    var msg = null;
+    var name = req.body.name;
+    var surname = req.body.surname;
+    var email = req.body.email;
+    var city = req.body.city;
+    var demoObj = new tblStudent({
+        name:name,
+        surname:surname,
+        email:email,
+        city:city
+    });
+    demoObj.save((err, data) => {
+        console.log(data);
+    })
+    res.redirect("/studentdata");
+});
+
+app.get("/student", (req, res) => {
+    tblStudent.find((err, data) => {
+        res.render("student", { data: data });
+    });
+})
+
+app.get("/studentdata",(req,res)=>{
+    tblStudent.find((err, data) => {
+        res.render("student", { data: data });
+    });   
+})
+
+app.post("/delete", (req, res) => {
+    var id = req.query.id;
+    var del = tblStudent.findByIdAndDelete(id)
+    del.exec((err,data)=>{
+        res.redirect("/studentdata");
     })
     
+});
+
+app.post("/updateData", (req, res) => {
+    var id = req.query.id;
+    tblStudent.find({ _id: id }, (err, data1) => {
+        if (!err) {
+            tblStudent.find((err, data) => {
+                if (!err) {
+                    res.render("updateView.ejs", { data: data, params: data1 });
+                }
+            })
+        }
+    });
+});
+
+app.post("/update", (req, res) => {
+    var id = req.query.id;
+    tblStudent.findOneAndUpdate({_id:id}, { name: req.body.name, surname: req.body.surname, city: req.body.city, email: req.body.email }, (err, data1) => {
+        if (!err) {
+            res.redirect("/studentdata");
+        }
+    });
 })
 
 app.listen(port, () => {
     console.log("Server is running on port number : ", port);
-})
+});
